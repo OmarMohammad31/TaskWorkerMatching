fad36bdc3cdf66930f9f96e4fb5e84f8e3c53ef1
Omar Mohammad Yousef <OmarMohammad31@hotmail.com>
2025-05-17 13:57:36 +0300

    Update TaskDTO.java
    reformatting methods

--- src/DataBase/DTO/TaskDTO.java
+++ src/DataBase/DTO/TaskDTO.java
@@ -14,65 +14,16 @@ public class TaskDTO
         this.AVGNEEDEDTIME = AVGNEEDEDTIME;
         this.FEE = FEE;
     }
-
     @Override
-    public String toString() {
-        return "TaskDTO{" +
-                "TID=" + TID +
-                ", Name='" + Name + '\'' +
-                ", SPECIALITY='" + SPECIALITY + '\'' +
-                ", AVGNEEDEDTIME=" + AVGNEEDEDTIME +
-                ", FEE=" + FEE +
-                '}';
-    }
-
-    public int getTID()
-    {
-        return TID;
-    }
-
-    public void setTID(int TID)
-    {
-        this.TID = TID;
-    }
-
-    public String getName()
-    {
-        return Name;
-    }
-
-    public void setName(String name)
-    {
-        Name = name;
-    }
-
-    public String getSPECIALITY()
-    {
-        return SPECIALITY;
-    }
-
-    public void setSPECIALITY(String SPECIALITY)
-    {
-        this.SPECIALITY = SPECIALITY;
-    }
-
-    public int getAVGNEEDEDTIME()
-    {
-        return AVGNEEDEDTIME;
-    }
-
-    public void setAVGNEEDEDTIME(int AVGNEEDEDTIME)
-    {
-        this.AVGNEEDEDTIME = AVGNEEDEDTIME;
-    }
-
-    public int getFEE()
-    {
-        return FEE;
-    }
-
-    public void setFEE(int FEE)
-    {
-        this.FEE = FEE;
-    }
+    public String toString() {return "TaskDTO{" + "TID=" + TID + ", Name='" + Name + '\'' + ", SPECIALITY='" + SPECIALITY + '\'' + ", AVGNEEDEDTIME=" + AVGNEEDEDTIME + ", FEE=" + FEE + '}'; }
+    public int getTID() {return TID;
+    public void setTID(int TID) {this.TID = TID; }
+    public String getName() {return Name;}
+    public void setName(String name) {Name = name;}
+    public String getSPECIALITY() {return SPECIALITY;}
+    public void setSPECIALITY(String SPECIALITY) {this.SPECIALITY = SPECIALITY;}
+    public int getAVGNEEDEDTIME() {return AVGNEEDEDTIME;}
+    public void setAVGNEEDEDTIME(int AVGNEEDEDTIME) {this.AVGNEEDEDTIME = AVGNEEDEDTIME;}
+    public int getFEE() {return FEE;}
+    public void setFEE(int FEE) {this.FEE = FEE;}
 }
